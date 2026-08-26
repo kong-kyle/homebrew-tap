024c3d647daac6d4f14a28271c824506413f4865
kylekonge <kong75067@gmail.com>
2026-08-26 21:58:22 +0800

    添加 WeChatTweak Homebrew 公式

---
+++ Formula/wechattweak.rb
@@ -0,0 +1,20 @@
+class Wechattweak < Formula
+  desc "A command-line tool for tweaking WeChat"
+  homepage "https://github.com/kong-kyle/WeChatTweak-kylekonge"
+  url "https://github.com/kong-kyle/WeChatTweak-kylekonge/releases/download/v1.0.0/wechattweak"
+  sha256 "1436107f7a6650b695fa9a6f6edaa84d09c69202e07def08cefbc4095d5b9856"
+  version "1.0.0"
+  license "AGPL-3.0"
+
+  depends_on macos: :monterey
+  depends_on arch: :arm64
+
+  def install
+    bin.install "wechattweak"
+    system "xattr", "-c", bin/"wechattweak"
+  end
+
+  test do
+    system bin/"wechattweak", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# WeChatTweak Homebrew Tap
+
+Install on Apple Silicon macOS:
+
+```bash
+brew install kong-kyle/tap/wechattweak
+```
+
+Update:
+
+```bash
+brew update
+brew upgrade wechattweak
+```
