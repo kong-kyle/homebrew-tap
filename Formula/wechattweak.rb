290c9ee2366abbab6506e84a93d931f32ca82b51
kylekonge <kong75067@gmail.com>
2026-08-26 22:31:22 +0800

    更新 WeChatTweak 修复版本

--- Formula/wechattweak.rb
+++ Formula/wechattweak.rb
@@ -1,8 +1,8 @@
 class Wechattweak < Formula
   desc "Command-line tool for tweaking WeChat"
   homepage "https://github.com/kong-kyle/WeChatTweak-kylekonge"
-  url "https://github.com/kong-kyle/WeChatTweak-kylekonge/releases/download/v1.0.0/wechattweak"
-  sha256 "1436107f7a6650b695fa9a6f6edaa84d09c69202e07def08cefbc4095d5b9856"
+  url "https://github.com/kong-kyle/WeChatTweak-kylekonge/releases/download/v1.0.1/wechattweak"
+  sha256 "0f088ee0c01dc72c4e275b846d53b05235269710f0403564073dc578bc290d1a"
   license "AGPL-3.0"
 
   depends_on arch: :arm64
